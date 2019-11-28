708022d3f5e96f6c2e11c187976c8fc41be978ea
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-28 05:18:58 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -6,6 +6,7 @@ attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
 :wrist_size, :glove_size, :pant_length, :pant_width
 
 def initialize(name: name)
-  
+  name.each {|key, value| self.send(("#{key}="), value)}
+  end
 
 end
